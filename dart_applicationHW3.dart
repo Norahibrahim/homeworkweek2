fa492f5bc3303511e5dc51de98b09d2a17a613c3
Norahibrahim <93065441+Norahibrahim@users.noreply.github.com>
2022-06-19 17:01:58 +0300

    Add files via upload

---
+++ dart_applicationHW3.dart
@@ -0,0 +1,16 @@
+void main() {    
+     print(greetings());  
+  int sum(int a, int b){       
+            int result;  
+            result = a+b;  
+            return result;  
+}  
+
+var c = sum(10,30);  
+print("The sum of two numbers is: ${c}");  
+}  
+
+ String greetings(){  
+   return "Welcome to dart"; 
+
+}  
